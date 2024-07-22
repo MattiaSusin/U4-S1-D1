8e7eade958ed36a8d2c17a810bd9b05e48649ddb
MattiaSusin <m.susin28@gmail.com>
2024-07-22 14:00:35 +0200

    Commit Esercizio 1

---
+++ src/Esercizio1.java
@@ -0,0 +1,47 @@
+public class Esercizio1 {
+    public static void main(String[] args) {
+
+        System.out.println("----------------MOLTIPLICA----------------");
+
+        int num1 = 8;
+        int num2 = 3;
+        int tot = num2 + num1;
+
+        System.out.println("il risultato della somma è:" + tot);
+
+        System.out.println("----------------CONCATENA----------------");
+
+        String concatenazione = "Siamo in:";
+
+        System.out.println(concatenazione + num1);
+
+        System.out.println("----------------INSERISCI----------------");
+
+        String[] strings = new String[5];
+        strings[0] = "Gian";
+        strings[1] = "Alberto";
+        strings[2] = "Marco";
+        strings[3] = "Federica";
+        strings[4] = "Anastasia";
+
+        String[] strings = new[5];
+
+        strings[2]= "Anna";
+
+        
+
+        String[] totStrings = new String[6];
+
+
+    }
+}
+
+
+/*
+ *-1---->MOLTIPLICA:accetta due interi e ritorna il loro prodotto
+ *
+ *-2---->CONCATENA:accetta una stringa e un intero e restituisce una stringa che concatena gli elementi
+ *
+ *-3---->INSERISCI:inserisci un array di stringhe di 5 elementi
+ *
+ * */
